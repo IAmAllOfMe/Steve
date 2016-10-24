1675dec97450a5f2fd5cf50f1f830880c1466bfb
Ben Norman <benanorman@live.com>
2016-10-24 17:39:44 +0100

    Commit 2. I have created a "Euler" function with a declaration in the header file and definition in the cpp file. Currently all it does is add two numbers together though. To be continued...

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,14 +9,29 @@
 
 using namespace std;
 
+void Euler(float x, float y) {
+	cout << "This will be the definition. Enter a number for x." << endl;
+	cin >> x;
+	cout << "Enter a number for y. The two numbers will be added together." << endl;
+	cin >> y;
+	cout <<" x + y = " << x + y << endl;
+
+}
 
 int main()
 {
 	cout << "If I have done this right then you should be reading these words now." << endl;
 	cout << "I plan on starting by creating a C++ maths function for the Euler method of integration" << endl;
-	cout << "To exit the program enter any integer and press enter." << endl;
+	cout << "Enter any integer and press enter." << endl;
 	int poo;
 	cin >> poo;
+	Euler(1,6);
+	cout << "Enter any integer and press enter to close the program" << endl;
+	int poop;
+	cin >> poop;
     return 0;
 }
 
+void Euler(float x, float y[])
+{
+}
---
+++ ConsoleApplication1/ConsoleApplication1/SteveHasAHeaderFile.h
@@ -0,0 +1,3 @@
+#pragma once
+/*Hopefully this header file will store the Euler method function I intend to create.*/
+void Euler(float x, float y[]);
