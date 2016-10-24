7d911924bfe6eb9627a91bc9b27f6eb65497daf0
Ben Norman <benanorman@live.com>
2016-10-24 16:42:08 +0100

    First commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "If I have done this right then you should be reading these words now." << endl;
+	cout << "I plan on starting by creating a C++ maths function for the Euler method of integration" << endl;
+	cout << "To exit the program enter any integer and press enter." << endl;
+	int poo;
+	cin >> poo;
+    return 0;
+}
+
