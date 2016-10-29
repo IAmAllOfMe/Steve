7b96464528e5c337d47b6fb4c8747fc3902ab67f
Ben Norman <benanorman@live.com>
2016-10-29 19:18:01 +0100

    Interesting things be happening

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
 	float G = 6.67*(10 ^ -11);//gravitational constant
 	float Ms = 1.99*(10 ^ 30);//mass of sun
@@ -20,23 +20,27 @@ int main()
 	float r = 1.50*(10 ^ 8); //initial distance = 1au. Value given here in km
 	float t = 0; //initial time is 0
 	float a; //a is acceleration
-	float v = 0;//initial velocity set to 0
+	float v = 1*(10^8);//initial velocity set to 0
 	float deltaT = 1000 * 3.15*(10 ^ 7); // 3.15*10^7 is the number of seconds in a year, so here deltaT = 1000yrs
-		int j;
+	int j;
 	for (j = 0; j < 5; j++)//we repeat the simulation 5 times
 	{
 		a = k / (r*r);
+		cout << "a =" << a << endl;
 		v = v + deltaT*a;
 		cout << "v = " << v << endl;
 		r = r + deltaT*v;
 		cout << "r = " << r << endl;
+		cout << "r/initial r =" << r / 1.5*(10 ^ 8) << endl;
+		cout << "NEXT ROUND" << endl;
+
 
 
 	}
-	int poo;
+	float poo;
 	cout << "Insert any integer the press enter to exit." << endl;
 	cin >> poo;
-return 0;
+	return 0;
 
-
+}
 
