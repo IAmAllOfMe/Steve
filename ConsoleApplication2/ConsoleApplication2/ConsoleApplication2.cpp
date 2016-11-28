36aa8352e406b1d416aeed4c8f99923cedf51fa5
Ben Norman <benanorman@live.com>
2016-11-28 18:03:37 +0000

    Attempted and failed to implement Runge-Kutta numerical integration. Half-finished attempt uploaded.

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -13,50 +13,100 @@ using namespace std;
 
 int main()
 {
-	float G = 6.67*pow(10,-11);//gravitational constant
-	float Ms = 1.99*pow(10,30);//mass of sun
-	float Me = 5.97*pow(10,24);//mass of earth
+	float G = 6.67*pow(10,-11);//gravitational constant in SI units
+	float Ms = 1.99*pow(10,30);//mass of sun in kg
+	float Me = 5.97*pow(10,24);//mass of earth in kg
 	float k = -G*(Ms+Me);//total constant for 2 body problem with 1 body of mass Me
-	float x = 1.50*pow(10,11), y = 0; //initial distance = 1au in x direction, 0 in y direction. Value given here in mm
+	float x = 1.50*pow(10,11), y = 0; 
+	//initial distance = 1au in x direction, 0 in y direction. Value given here in mm
 	float t = 0; //initial time is 0
 	float ax, ay; //a is acceleration
-	float vx = 0, vy = 30000;//initial velocity set to 0 in x direction, 30 km/s in y directio 
-	float deltaT = 48*3600; // deltaT is measured in seconds; 3600 is the number of seconds in an hour; 48 is the number of hours in two days, so deltaT = 2 days 
+	float vx = 0, vy = 30000;
+	//initial velocity set to 0 in x direction, 30 km/s in y direction. 
+	//This is the speed of the Earth's orbit 
+	float deltaT = 24*3600;
+	// deltaT is measured in seconds; 3600 is the number of seconds in an hour; 
+	//48 is the number of hours in two days, so deltaT = 2 days 
+	float dx1 = 0, dx2 = 0, dx3 = 0, dx4, dy1 = 0, dy2 = 0, dy3 = 0, dy4, dx, dy;
+	float dvx1, dvx2, dvx3, dvx4, dvy1, dvy2, dvy3, dvy4, dvx, dvy;
 	float r = sqrt((x*x) + (y*y));// r is the straight line distance between the two bodies
+	//Now we go mental with Runge-Kutta stuff
+	float ddx1 = x + dx1/2, ddy1 = y + dy1/2;
+	float ddx2 = x + dx2/2, ddy2 = y + dy2 /2;
+	float ddx3 = x + dx3 / 2, ddy3 = y + dy3/2;
+	float dr1 = sqrt((ddx1*ddx1) + (ddy1*ddy1));
+	float dr2 = sqrt((ddx2*ddx2) + (ddy2*ddy2));
+	float dr3 = sqrt((ddx3*ddx3) + (ddy3*ddy3));
+	//loop time
 	int j;
 	ofstream mehfile;
 	mehfile.open("data.txt", ios::trunc);
 	ofstream secondfile;
-		secondfile.open("data2.txt");
-	cout << "k = "<< k << endl;
-	for (j = 0; j < 190; j++)//we repeat the simulation 5 times
+		secondfile.open("data2.txt");//save meh data pls
+   cout << "k = "<< k << endl;
+	for (j = 0; j < 380000; j++)//we repeat the simulation 190 times (just over a year)
 		
 	{
-		cout << "ROUND " << j + 1 << endl;
+		//cout << "ROUND " << j + 1 << endl;
 		ax = (k*x) / pow(r, 3);
-		cout << "ax = " << ax << endl;
+		//cout << "ax = " << ax << endl;
 		ay = (k*y) / pow(r, 3);
-		cout << "ay = " << ay <<endl;
+		//cout << "ay = " << ay <<endl;
+
+		//dx1 = deltaT*vx;
+		//dy1 = deltaT*vy;
+		//dvx1 = deltaT*ax;
+		//dvy1 = deltaT*ay;
+
+		//dx2 = deltaT*(vx + 0.5*dvx1);
+		//dy2 = deltaT*(vy + 0.5*dvy1);
+		//dvx2 = deltaT*((k*ddx1) / pow(dr1, 3));
+		//dvy2 = deltaT*((k*ddy1) / pow(dr1, 3));
+		//in other words dx2 is deltaT*a when x = x + dx1/2
+
+		//dx3 = deltaT*(vx + 0.5*dvx2);
+		//dy3 = deltaT*(vy + 0.5*dvy2);
+		//dvx3 = deltaT*((k*ddx2) / pow(dr2, 3));
+		//dvy3 = deltaT*((k*ddy2) / pow(dr2, 3));
+
+		//final step
+		//dx4 = deltaT*(vx + 0.5*dvx3);
+		//dy4 = deltaT*(vy + 0.5*dvy3);
+		//dvx4 = deltaT*((k*ddx3) / pow(dr3, 3));
+		//dvy4 = deltaT*((k*ddy3) / pow(dr3, 3));
+
+		//dx = (dx1 + 2 * dx2 + 2 * dx3 + dx4) / 6;
+		//dy = (dy1 + 2 * dy2 + 2 * dy3 + dy4) / 6;
+		//dvx = (dvx1 + 2 * dvx2 + 2 * dvx3 + dvx4) / 6;
+		//dvy = (dvy1 + 2 * dvy2 + 2 * dvy3 + dvy4) / 6;
+
+		//x = x + dx;
+		//y = y + dy;
+		//vx = vx + dvx;
+		//vy = vy + dvy;
+		//ignore the old Euler stuff. Ew. 	
 		vx = vx + deltaT*ax;
-		cout << "vx = " << vx << endl;
+		//cout << "vx = " << vx << endl;
 		vy = vy + deltaT*ay;
-		cout << "vy = " << vy << endl;
-		x = x + deltaT*vx;
-		cout << "x = " << x << endl;
+		//cout << "vy = " << vy << endl;
+	    x = x + deltaT*vx;
+		//cout << "x = " << x << endl;
 		y = y + deltaT*vy;
-		cout << "y = " << y << endl;
+		//cout << "y = " << y << endl;
 		r = sqrt((x*x) + (y*y));
-		cout << "r = " << r << endl;
+		//cout << "r = " << r << endl;
 		
 		mehfile << x << " " << endl;
 		secondfile << y << endl;
+		
 	}
+	cout << "r = " << r << endl;
 	mehfile.close();
 	secondfile.close();
-	float poo;
+	int integer;
 	cout << "Insert any integer the press enter to exit." << endl;
-	cin >> poo;
+	cin >> integer;
 	return 0;
-
+	
 }
 
