29edf890d6a859c43ad8f1f83a347196f244a48a
Ben Norman <benanorman@live.com>
2017-01-24 17:25:00 +0000

    edited to be more accurate, almost done now!

--- ConsoleApplication3/ConsoleApplication3/Project.cpp
+++ ConsoleApplication3/ConsoleApplication3/Project.cpp
@@ -21,29 +21,36 @@ float Ms = 1.99*pow(10, 30);//mass of sun in kg
 float Me = 5.97*pow(10, 24);//mass of earth in kg
 float ty = 3.145*pow(10, 7);//number of seconds in a year
 float au = 1.49598*pow(10, 11); //one Astronomical Unit converted into metres
-float H = 0.05*au;//height of disk
 float pi = 3.1415926536;
 float n = 1;
-float s1 = 2700; // in g/cm^2
+float r20 = 3 * au;//initial outer disc inner edge
+float s1 = 2700;// in g/cm^2
 //s stands for sigma in the Terquem paper
 
 //Declare acceleration functions
-double accelx(double x, double y, double vx, double vy);
-double accely(double x, double y, double vx, double vy);
+double accelx(double x, double y, double vx, double vy, double t);
+double accely(double x, double y, double vx, double vy, double t);
 
 //Define function for accleration in x direction
-double accelx(double x, double y, double vx, double vy) {
+double accelx(double x, double y, double vx, double vy, double t) {
 	double r = sqrt((x*x) + (y*y));
-	
-	double s = 0.1*(s1 /r)*au;
+	double H = 0.05*r;
+	double s;
+	if (9 * au < r < 11 * au) {
+	 s = 0;
+	}
+	else {
+		 s = 0.1*(s1 / r)*au;
+	}
 	//using equations 1 and 7 in the Terquem paper
 
-	double tmig = ty*(2 / (2.7+(1.1*n)))*(Ms / Me)*pow(H / r, 2)*(Ms / (2 * pi*s1*s1))*pow(au / au, 0.5);
+	double tmig = ty*(2 / (2.7+(1.1*n)))*(Ms / Me)*pow(H / r, 2)*(Ms / (2 * pi*s*s))*pow(au / au, 0.5);
 		//assuming eccentricity = 0
-	double tecc = ty*0.1*(Ms/Me)*pow(H/r,4)*(Ms / (2 * pi*s1*s1))*pow(au / au, 0.5);
+	double tecc = ty*0.1*(Ms/Me)*pow(H/r,4)*(Ms / (2 * pi*s*s))*pow(au / au, 0.5);
 	//assuming eccentricity = 0
 	double ax;
-	if(9*au < r < 11*au){
+	double r2 = r20 + (10 * au - r20)*t / (3.2*pow(10, 6));
+	if(r2-(au/2) < r < r2+(au/2)){
 	 ax = (-G*(Ms + Me)*x) / pow(r, 3) + vx / tmig + (2 / (pow(r, 2)*tecc))*(vx*x + vy*y)*x;
 	}
 	else {
@@ -52,21 +59,29 @@ double accelx(double x, double y, double vx, double vy) {
 	return ax;
 }
 //Define function for accleration in y direction
-double accely(double x, double y, double vx, double vy) {
+double accely(double x, double y, double vx, double vy, double t) {
 	double r = sqrt((x*x) + (y*y));
-	
-	double s = 0.1*(s1 / r)*au;
+	double H = 0.05*r;
+	double s;
+	if (9 * au < r < 11 * au) {
+		s = 0;
+	}
+	else {
+		s = 0.1*(s1 / r)*au;
+	}
+
 	//using equations 1 and 7 in the Terquem paper
 
-	double tmig = ty*(2 / 3.8)*(Ms / Me)*pow(H / r, 2)*(Ms / (2 * pi*s1*s1))*pow(au / au, 0.5);
+	double tmig = ty*(2 / 3.8)*(Ms / Me)*pow(H / r, 2)*(Ms / (2 * pi*s*s))*pow(au / au, 0.5);
 	//assuming eccentricity = 0
 	//tmig is in years, so we multiply by ty to give it's value in seconds
 	
-	double tecc = ty*0.1*(Ms / Me)*pow(H / r, 4)*(Ms / (2 * pi*s1*s1))*pow(au / au, 0.5);
+	double tecc = ty*0.1*(Ms / Me)*pow(H / r, 4)*(Ms / (2 * pi*s*s))*pow(au / au, 0.5);
 	//assuming eccentricity = 0
 	//tecc is in years, so we multiply by ty to give it's value in seconds
 	double ay;
-	if(9*au < r < 11*au){
+	double r2 = r20 + (10 * au - r20)*t / (3.2*pow(10, 6));
+	if(r2-(au/2) < r < r2+(au/2)){
 	 ay = (-G*(Ms + Me)*y) / pow(r, 3) + vy / tmig + (2 / (pow(r, 2)*tecc))*(vx*x + vy*y)*y;
 	}
 	else {
@@ -101,12 +116,15 @@ int main()
 	ofstream secondfile;
 	secondfile.open("yvalues.txt");
 
+	double t;//time passed in years
 	int j;
 	for (j = 0; j < 2* 365400; j++) {
 		//cout << "ROUND " << j + 1 << endl;
+		//j = 12 hours
+		t = j * 3600 / ty;
 
-		ax = accelx(x, y, vx, vy);
-		ay = accely(x, y, vx, vy);
+		ax = accelx(x, y, vx, vy, t);
+		ay = accely(x, y, vx, vy, t);
 		
 		dx1 = h*vx;
 		dy1 = h*vy;
@@ -115,18 +133,18 @@ int main()
 
 		dx2 = h*(vx + (dvx1 / 2));
 		dy2 = h*(vy + (dvy1 / 2));
-		dvx2 = h*accelx(x + dx1 / 2, y + dy1 / 2, vx + dvx1 / 2, y + dvy1 / 2);
-		dvy2 = h*accely(x + dx1 / 2, y + dy1 / 2, x + dvx1 /  2, vy + dvy1 / 2);
+		dvx2 = h*accelx(x + dx1 / 2, y + dy1 / 2, vx + dvx1 / 2, y + dvy1 / 2, t);
+		dvy2 = h*accely(x + dx1 / 2, y + dy1 / 2, x + dvx1 /  2, vy + dvy1 / 2, t);
 
 		dx3 = h*(vx + (dvx2 / 2));
 		dy3 = h*(vy + (dvy2 / 2));
-		dvx3 = h*accelx(x + dx2 / 2, y + dy2 / 2, vx + dvx2 / 2, vy + dvy2 / 2);
-		dvy3 = h*accely(x + dx2 / 2, y + dy2 / 2, vx + dvx2 / 2, vy + dvy2 / 2);
+		dvx3 = h*accelx(x + dx2 / 2, y + dy2 / 2, vx + dvx2 / 2, vy + dvy2 / 2, t);
+		dvy3 = h*accely(x + dx2 / 2, y + dy2 / 2, vx + dvx2 / 2, vy + dvy2 / 2, t);
 
 		dx4 = h*(vx + dvx3);
 		dy4 = h*(vy + dvy3);
-		dvx4 = h*accelx(x + dx3, y + dy3, vx + dvx3 / 2, vy + dvy3 / 2);
-		dvy4 = h*accely(x + dx3, y + dy3, vx + dvx3 / 2, vy + dvy3 / 2);
+		dvx4 = h*accelx(x + dx3, y + dy3, vx + dvx3 / 2, vy + dvy3 / 2, t);
+		dvy4 = h*accely(x + dx3, y + dy3, vx + dvx3 / 2, vy + dvy3 / 2, t);
 
 		dx = (dx1 + 2 * dx2 + 2 * dx3 + dx4) / 6;
 		dy = (dy1 + 2 * dy2 + 2 * dy3 + dy4) / 6;
@@ -138,7 +156,7 @@ int main()
 		vx = vx + dvx;
 		vy = vy + dvy;
 		r = sqrt((x*x) + (y*y));
-		cout << "r = " << r << endl;
+		cout << "r = " << r/au << " AU" << endl;
 
 		//storing generated x and y values
 		mehfile << x << endl;
